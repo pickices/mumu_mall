93dce3364b03dad3d3cbc28819b170724b668440
pickices <pickices@qq.com>
2021-06-24 02:11:28 +0800

    initialized

---
+++ src/main/java/com/liuxinchi/mumu_mall/MumuMallApplication.java
@@ -0,0 +1,13 @@
+package com.liuxinchi.mumu_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumuMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumuMallApplication.class, args);
+    }
+
+}
